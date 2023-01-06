dfc877b694252f418bc7f23e6dc9ca257398ee46
장성민 <aristatait@gmail.com>
2023-01-06 13:41:32 +0900

    feat: api sample

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}: AppProps) {
+
+  const queryClient = new QueryClient()
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
 }
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,30 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
+export interface Person {
+  birthday?: string,
+  known_for_department?: string,
+  deathday?: string,
+  id?: number,
+  name?: string,
+  also_known_as?: string[],
+  gender?: number,
+  biography?: string,
+  popularity?: number,
+  place_of_birth?: string,
+  profile_path?: string,
+  adult?: boolean,
+  imdb_id?: string,
+  homepage?: string
 }
-
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Person>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+
+  const url = `https://api.themoviedb.org/3/person/550?api_key=${process.env.MOVIE_API_KEY}`
+  console.log("url", url);
+  const result:Person = await (await fetch(url)).json()
+
+  res.status(200).json({...result, name: "Aristata"})
 }
